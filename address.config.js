5f1f8d9719039186721bf6f4a943ec67b86e691e
Roberto García <roberto@rhizomik.net>
2024-10-28 10:46:12 +0100

    feat: adapt to AgrospAI marketplace

--- address.config.js
+++ address.config.js
@@ -8,23 +8,26 @@ module.exports = {
   featured: [
     {
       title: 'AI-based Animal Well-being Assessment without Images Leakage',
+      title: 'AI-based Animal Well-being Assessment without Images Leakage',
       assets: [
         // Mask R-CNN segmentation & Tracking
-        'did:op:2c0e29e89bee015cbbbaa46ce7f9a566a2cb541367bca926c5552a1b2d697892',
+        'did:op:60d977086cc8e499b996c3a50e1f2f57c023e25b77db58b394beb70b10fdfd21',
         // CEP Pigs Images
-        'did:op:31d6d1ea0fc540e1ea6e5268ebfd53e8129992cd6971dfbbbd0b88b08ca6f939'
+        'did:op:42bac83af0e801f25ccaa3cffe8db3afa1bff9a94be9c12388b95feb641dc5a6'
       ]
     },
     {
       title:
         'Precision Pig Feeding Semantic Data Integration and Sovereign Data Pooling',
+      title:
+        'Precision Pig Feeding Semantic Data Integration and Sovereign Data Pooling',
       assets: [
         // CEP's CSV Data Mapper and Semantic Data Pooler
-        'did:op:d20f956e79709fb2469fffe2bd85cf2fec95a21d2497998bb530043c6bbec901',
+        'did:op:ffa62c4e6306d24c68d3a0f0825804045abb91f27a12f5452403cf6b804ca519',
         // CEP Pigs Feeding Data
-        'did:op:f7946c46eb87318b2cd34efdd5f33b19ea9223a90b67f447da6a92aa68ca007c',
+        'did:op:c898901a7133b6ec92e871d9d692073d18effb0f4d5fe955d3f8f0a7b9f8b962',
         // Exploratory Data Analysis
-        'did:op:34d5f73d77550843201ee1a43ad9d404d3e557ed6a70772e9afde7a27d863b8f'
+        'did:op:80d669824854177e42fe4e23f42ba5f7e9823d8ac6f9f224fec157e25d5f04da'
       ]
     }
   ],
