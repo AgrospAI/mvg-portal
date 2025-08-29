2cffab5b74d8d73899157223483926f447edf27d
spin3l <dev.christianlopezgarcia@gmail.com>
2025-08-29 11:47:33 +0200

    impl: better error messages

--- src/components/VerifiableCredentials/index.tsx
+++ src/components/VerifiableCredentials/index.tsx
@@ -1,20 +1,23 @@
 import ExcludingAccordion from '@context/ExcludingAccordion'
-import { Suspense, useRef } from 'react'
+import { ReactNode, Suspense, useRef } from 'react'
 import styles from './index.module.css'
 import { VerifiableCredential } from './VertifiableCredential'
 
 interface VerifiableCredentialsProps {
   credentials: string[]
+  children?: ReactNode
 }
 
 /**
  * Renders a list of verifiable credentials from the same entity.
  *
- * @param {string[]} children Renders a list of verifiable credentials from the same entity.
+ * @param {string[]} credentials Renders a list of verifiable credentials from the same entity.
+ * @param {ReactNode?} children Before rendering the list of credentials "header-like"
  * @returns {JSX.Element}
  */
 export const VerifiableCredentials = ({
-  credentials
+  credentials,
+  children
 }: Readonly<VerifiableCredentialsProps>): JSX.Element => {
   const refs = useRef<(HTMLElement | null)[]>([])
 
@@ -28,29 +31,35 @@ export const VerifiableCredentials = ({
   }
 
   return (
-    <Suspense fallback="Loading">
-      <ExcludingAccordion>
-        <div className={styles.verifiableCredentials}>
-          {credentials.map((credential, index) => (
-            <section key={credential} className={styles.verifiableCredentials}>
-              <ExcludingAccordion.Trigger
-                ref={(el) => (refs.current[index] = el)}
-                index={index}
-                openCallback={() => openCallback(index)}
+    <>
+      {children}
+      <Suspense fallback="Loading">
+        <ExcludingAccordion>
+          <div className={styles.verifiableCredentials}>
+            {credentials.map((credential, index) => (
+              <section
+                key={credential}
+                className={styles.verifiableCredentials}
               >
-                <h4 className={styles.credentialTitle}>
-                  Verifiable credential Nº{index}
-                </h4>
-              </ExcludingAccordion.Trigger>
-              <ExcludingAccordion.Content index={index}>
-                <div className={styles.content}>
-                  <VerifiableCredential url={credential} />
-                </div>
-              </ExcludingAccordion.Content>
-            </section>
-          ))}
-        </div>
-      </ExcludingAccordion>
-    </Suspense>
+                <ExcludingAccordion.Trigger
+                  ref={(el) => (refs.current[index] = el)}
+                  index={index}
+                  openCallback={() => openCallback(index)}
+                >
+                  <button className={styles.credentialTitle}>
+                    Verifiable credential Nº{index}
+                  </button>
+                </ExcludingAccordion.Trigger>
+                <ExcludingAccordion.Content index={index}>
+                  <div className={styles.content}>
+                    <VerifiableCredential url={credential} />
+                  </div>
+                </ExcludingAccordion.Content>
+              </section>
+            ))}
+          </div>
+        </ExcludingAccordion>
+      </Suspense>
+    </>
   )
 }
