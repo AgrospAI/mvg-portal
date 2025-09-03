8ef540971c607cd61b83fff73432397aa8d42c70
spin3l <dev.christianlopezgarcia@gmail.com>
2025-09-03 16:12:06 +0200

    fixed merge

--- src/server/di/container.ts
+++ src/server/di/container.ts
@@ -1,22 +1,6 @@
 'use server'
+
 import dotenv from 'dotenv'
-
-import { Container } from 'inversify'
-import ICredentialsService from '../credentials/credentials'
-import { RedisCredentialsService } from '../credentials/impl/redis-credentials'
-dotenv.config()
-
-const container = new Container()
-
-container
-  .bind<ICredentialsService>('Credentials')
-  .to(RedisCredentialsService)
-  .inSingletonScope()
-
-export { container }
-;('use server')
-import dotenv from 'dotenv'
-
 import { Container } from 'inversify'
 import IConsentsService from '../consents/consents'
 import IConsentResponseService from '../consents/consents-response'
