7d3c69eab0311bc8380d7bbfdb3ea025fa45558a
spin3l <dev.christianlopezgarcia@gmail.com>
2025-09-05 09:37:24 +0200

    fix: health endpoint

--- src/@utils/consents/routes.ts
+++ src/@utils/consents/routes.ts
@@ -1,7 +1,7 @@
 export const ConsentsApiRoutes = {
   ConsentsResponse: (id: string) => `consents-response/${id}`,
   Consents: (id: string) => `consents/${id}`,
-  ConsentsHealth: 'consents/health',
+  ConsentsHealth: 'health/consents',
   UserConsentsAmount: (address: string) => `user/${address}/consents-amount`,
   UserConsents: (address: string) => `user/${address}/consents`
 }
