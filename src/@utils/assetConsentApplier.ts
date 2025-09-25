427c16b661a6679c2c93c8b61cc73bc699cf10f5
spin3l <dev.christianlopezgarcia@gmail.com>
2025-09-25 10:07:35 +0200

    fix: consents application in blockchain

--- src/@utils/assetConsentApplier.ts
+++ src/@utils/assetConsentApplier.ts
@@ -9,6 +9,7 @@ import { Signer } from 'ethers'
 import { Address } from 'wagmi'
 import { createTrustedAlgorithmList } from './compute'
 import { extractDidFromUrl } from './consents/utils'
+import { PublisherTrustedAlgorithm } from '@oceanprotocol/lib'
 
 interface AssetUpdater {
   update(asset: AssetExtended): Promise<void>
@@ -20,9 +21,9 @@ const RequestTrustedAlgorithmPublisherUpdater = (
   update: async (asset: AssetExtended) =>
     asset.services
       .filter(({ type }) => type === 'compute')
-      .forEach((service) => {
+      .forEach((service) =>
         service.compute.publisherTrustedAlgorithmPublishers.push(publisherDid)
-      })
+      )
 })
 
 const RequestTrustedAlgorithmUpdater = (
@@ -30,20 +31,30 @@ const RequestTrustedAlgorithmUpdater = (
   newCancelToken: () => CancelToken
 ): AssetUpdater => ({
   update: async (asset: AssetExtended) => {
-    const publisherTrustedAlgorithms = await createTrustedAlgorithmList(
-      [algorithmDid],
-      asset.chainId,
-      newCancelToken()
+    await Promise.all(
+      asset.services
+        .filter(({ type }) => type === 'compute')
+        .map(async (service) => {
+          const newAlgorithmList = await createTrustedAlgorithmList(
+            [algorithmDid],
+            asset.chainId,
+            newCancelToken()
+          )
+          service.compute.publisherTrustedAlgorithms = [
+            ...(service.compute.publisherTrustedAlgorithms || []),
+            ...newAlgorithmList
+          ].filter(
+            // Only keep one copy of each algorithm
+            (
+              algo: PublisherTrustedAlgorithm,
+              index: number,
+              self: PublisherTrustedAlgorithm[]
+            ) => self.findIndex((a) => a.did === algo.did) === index
+          )
+
+          return Promise.resolve()
+        })
     )
-
-    const computeIndex = asset.services.findIndex((s) => s.type === 'compute')
-    if (computeIndex !== -1) {
-      asset.services[computeIndex].compute.publisherTrustedAlgorithms.push(
-        ...publisherTrustedAlgorithms
-      )
-    }
-
-    return Promise.resolve()
   }
 })
 
@@ -51,27 +62,21 @@ const RequestAllowNetworkAccessUpdater = (): AssetUpdater => ({
   update: async (asset: AssetExtended) =>
     asset.services
       .filter(({ type }) => type === 'compute')
-      .forEach((service) => {
-        service.compute.allowNetworkAccess = true
-      })
+      .forEach((service) => (service.compute.allowNetworkAccess = true))
 })
 
 const Updater = (consent: Consent, newCancelToken: () => CancelToken) => ({
-  get: (permission: keyof PossibleRequests): AssetUpdater => {
-    switch (permission) {
-      case 'allow_network_access':
-        return RequestAllowNetworkAccessUpdater()
-      case 'trusted_algorithm':
-        return RequestTrustedAlgorithmUpdater(
-          extractDidFromUrl(consent.algorithm),
-          newCancelToken
-        )
-      case 'trusted_algorithm_publisher':
-        return RequestTrustedAlgorithmPublisherUpdater(
-          consent.solicitor.address
-        )
-    }
-  }
+  get: (permission: keyof PossibleRequests): AssetUpdater =>
+    ({
+      allow_network_access: RequestAllowNetworkAccessUpdater(),
+      trusted_algorithm: RequestTrustedAlgorithmUpdater(
+        extractDidFromUrl(consent.algorithm),
+        newCancelToken
+      ),
+      trusted_algorithm_publisher: RequestTrustedAlgorithmPublisherUpdater(
+        consent.solicitor.address
+      )
+    }[permission])
 })
 
 export const AssetConsentApplier = (
@@ -82,25 +87,20 @@ export const AssetConsentApplier = (
   newCancelToken: () => CancelToken,
   newAbortSignal: () => AbortSignal
 ) => ({
-  apply: async (asset: AssetExtended): Promise<boolean> => {
-    const consents = Object.keys(response.permitted)
-    await Promise.all(
-      consents
+  apply: async (asset: AssetExtended): Promise<boolean> =>
+    Promise.all(
+      Object.keys(response.permitted)
         .filter((key) => response.permitted[key])
         .map((key: keyof PossibleRequests) =>
           Updater(consent, newCancelToken).get(key).update(asset)
         )
+    ).then(async () =>
+      setNFTMetadataAndTokenURI(
+        asset,
+        accountId,
+        signer,
+        decodeTokenURI(asset.nft.tokenURI),
+        newAbortSignal()
+      ).then(async (tx) => (tx ? await tx.wait().then(() => true) : false))
     )
-
-    const setMetadataTx = await setNFTMetadataAndTokenURI(
-      asset,
-      accountId,
-      signer,
-      decodeTokenURI(asset.nft.tokenURI),
-      newAbortSignal()
-    )
-
-    if (!setMetadataTx) return false
-    return setMetadataTx.wait().then(() => true)
-  }
 })
