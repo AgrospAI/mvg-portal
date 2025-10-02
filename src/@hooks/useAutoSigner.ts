0f0a9c5fd5454274ee67844fe5c8f832da76fbb0
spin3l <dev.christianlopezgarcia@gmail.com>
2025-10-02 16:58:18 +0200

    fix: Working network switching

--- src/@hooks/useAutoSigner.ts
+++ src/@hooks/useAutoSigner.ts
@@ -1,41 +1,26 @@
 import { useAutomation } from '@context/Automation/AutomationProvider'
-import { ethers, Signer } from 'ethers'
-import { useCallback, useEffect, useState } from 'react'
-import { Address, useAccount, useSigner } from 'wagmi'
+import { useMemo } from 'react'
+import { Address, useAccount, useProvider, useSigner } from 'wagmi'
 
 export const useAutoSigner = () => {
   const { address } = useAccount()
   const { data: signer } = useSigner()
   const { autoWallet, isAutomationEnabled } = useAutomation()
+  const wagmiProvider = useProvider()
 
-  const [signerToUse, setSignerToUse] = useState<Signer | undefined>(signer)
-  const [accountToUse, setAccountToUse] = useState<Address | undefined>(address)
-
-  const getAutoSigner = useCallback((): Signer | undefined => {
-    let selectedSigner: Signer | undefined
-
-    const provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
+  const { signerToUse, accountToUse } = useMemo(() => {
     if (isAutomationEnabled && autoWallet) {
-      selectedSigner = autoWallet.connect(provider)
-    } else {
-      selectedSigner = signer
+      const connected = autoWallet.connect(wagmiProvider)
+      return {
+        signerToUse: connected,
+        accountToUse: autoWallet.address as Address
+      }
     }
+    return {
+      signerToUse: signer,
+      accountToUse: address
+    }
+  }, [isAutomationEnabled, autoWallet, signer, address, wagmiProvider])
 
-    // Update local state for convenience
-    setSignerToUse(selectedSigner)
-    setAccountToUse(
-      isAutomationEnabled && autoWallet?.address
-        ? (autoWallet.address as Address)
-        : address
-    )
-
-    return selectedSigner
-  }, [isAutomationEnabled, autoWallet, address, signer])
-
-  // Update signer whenever dependencies change (network, wallet, automation)
-  useEffect(() => {
-    getAutoSigner()
-  }, [getAutoSigner])
-
-  return { signer: signerToUse, accountId: accountToUse, getAutoSigner }
+  return { signer: signerToUse, accountId: accountToUse }
 }
