c22eb7200be071bac0e3a5472e11e706acc392a3
Aleix Segura <aleixsegura29@gmail.com>
2025-12-17 13:34:25 +0100

    fix: form component name

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -3,14 +3,14 @@ import Form from '../components/Form'
 import Page from '../components/@shared/Page'
 import Head from 'next/head'
 
-export default function TestPage(): ReactElement {
+export default function PageForm(): ReactElement {
   return (
     <>
       <Head>
         <meta name="robots" content="noindex, nofollow"></meta>
       </Head>
       <Page
-        title="Cuestionario de Evaluación Empresarial"
+        title="Cuestionario de Evaluación de Madurez Digital Empresarial"
         description="Página temporal para validar el formulario de cuestionario"
         uri="/test"
       >
